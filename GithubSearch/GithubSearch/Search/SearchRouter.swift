01c3f61e7ec703c835f4bffdba8ef3d99190f098
kim so yeon <sykim@autocrpt.io>
2023-11-10 16:09:25 +0900

    chore : change folder name and structure

---
+++ GithubSearch/GithubSearch/Search/SearchRouter.swift
@@ -0,0 +1,26 @@
+//
+//  SearchRouter.swift
+//  GithubSearch
+//
+//  Created by 김소연 on 11/10/23.
+//
+
+import RIBs
+
+protocol SearchInteractable: Interactable {
+    var router: SearchRouting? { get set }
+    var listener: SearchListener? { get set }
+}
+
+protocol SearchViewControllable: ViewControllable {
+    // TODO: Declare methods the router invokes to manipulate the view hierarchy.
+}
+
+final class SearchRouter: ViewableRouter<SearchInteractable, SearchViewControllable>, SearchRouting {
+
+    // TODO: Constructor inject child builder protocols to allow building children.
+    override init(interactor: SearchInteractable, viewController: SearchViewControllable) {
+        super.init(interactor: interactor, viewController: viewController)
+        interactor.router = self
+    }
+}
